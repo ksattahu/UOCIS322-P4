baa95559db6dd85131e38328ff18cf2134651a5d
unknown <you@example.com>
2021-05-16 23:14:22 -0700

    Completed P4 to specs

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -12,7 +12,7 @@ import arrow
 #  these signatures even if you don't use all the
 #  same arguments.
 #
-
+BREVET = [(200,15,34),(400,15,32),(600,15,30),(1000,11.428,28)]
 
 def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
     """
@@ -26,7 +26,24 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        A date object indicating the control open time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+    if(control_dist_km == 0):
+        return brevet_start_time
+    sum = 0
+    last = 0
+    for b in BREVET:
+        if (brevet_dist_km == 300) and (control_dist_km >= 300):
+             return brevet_start_time.shift(hours=9)
+        elif b[0] >= control_dist_km:
+            sum += (control_dist_km - last) / b[2]
+            minutes = round((sum - (sum//1))*60)
+            return brevet_start_time.shift(hours=(sum//1), minutes=minutes)
+        elif (1.2 * brevet_dist_km) >= control_dist_km and (b[0] >= brevet_dist_km):
+            sum += (b[0] - last)/ b[2]
+            minutes = round((sum - (sum//1))*60)
+            return brevet_start_time.shift(hours=(sum//1), minutes=minutes)
+        elif b[0] < control_dist_km:
+            sum += (b[0] - last) / b[2]
+            last = b[0]
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -41,4 +58,34 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        A date object indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+    sum = 0
+    last = 0
+    for b in BREVET:
+        if(control_dist_km <= 60):
+            sum += (control_dist_km)/ 20
+            minutes = round((sum - (sum//1))*60)
+            return brevet_start_time.shift(hours=1+(sum//1), minutes=round(minutes))
+        elif (brevet_dist_km == 300) and (control_dist_km >= 300):
+             return brevet_start_time.shift(hours=20)
+        elif b[0] > control_dist_km:
+            sum += (control_dist_km - last) / b[1]
+            minutes = round((sum - (sum//1))*60)
+            return brevet_start_time.shift(hours=(sum//1), minutes=round(minutes))
+        elif (1.2 * brevet_dist_km) >= control_dist_km and (b[0] >= brevet_dist_km):
+            if(brevet_dist_km == 200):
+                return brevet_start_time.shift(hours=13, minutes=30)
+            elif(brevet_dist_km == 300):
+                return brevet_start_time.shift(hours=20)
+            elif(brevet_dist_km == 400):
+                return brevet_start_time.shift(hours=27)
+            elif(brevet_dist_km == 600):
+                return brevet_start_time.shift(hours=40)
+            elif(brevet_dist_km == 1000):
+                return brevet_start_time.shift(hours=75)
+        elif b[0] == control_dist_km:
+            sum += (control_dist_km - last) / b[1]
+            minutes = round((sum - (sum//1))*60)
+            return brevet_start_time.shift(hours=(sum//1), minutes=round(minutes))
+        elif b[0] < control_dist_km:
+            sum += (b[0] - last) / b[1]
+            last = b[0]
--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -51,15 +51,18 @@ def _calc_times():
     """
     app.logger.debug("Got a JSON request")
     km = request.args.get('km', 999, type=float)
+    brevet_dist_km = request.args.get('brevet_dist_km', 999, type=int)
+    begin_date = request.args.get('begin_date', type=str)
     app.logger.debug("km={}".format(km))
     app.logger.debug("request.args: {}".format(request.args))
+    begin_date = arrow.get(begin_date, "YYYY-MM-DDTHH:mm").shift(hours=8)
     # FIXME!
     # Right now, only the current time is passed as the start time
     # and control distance is fixed to 200
     # You should get these from the webpage!
-    open_time = acp_times.open_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
-    close_time = acp_times.close_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
-    result = {"open": open_time, "close": close_time}
+    open_time = acp_times.open_time(km, brevet_dist_km, begin_date) #arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
+    close_time = acp_times.close_time(km, brevet_dist_km, begin_date) #arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
+    result = {"open": open_time.isoformat().format('YYYY-MM-DDTHH:mm'), "close": close_time.isoformat().format('YYYY-MM-DDTHH:mm')}
     return flask.jsonify(result=result)
 
 
